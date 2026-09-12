e6c6c1f30a53d34e7ec052db8043c9242a2587b8
Akshay Kumar <akshaysharmav7@gmail.com>
2026-09-12 10:49:16 +0530

    init

---
+++ src/main/java/org/app/clinic_management_platform/ClinicManagementPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.app.clinic_management_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicManagementPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicManagementPlatformApplication.class, args);
+    }
+
+}
